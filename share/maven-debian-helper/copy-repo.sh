e0bac4122d61cab08914924fc198cfd7b1691c56
Torsten Werner <twerner@debian.org>
2009-01-29 21:31:32 +0000

    UNRELEASED * Implement pom faking.

--- share/maven-debian-helper/copy-repo.sh
+++ share/maven-debian-helper/copy-repo.sh
@@ -5,12 +5,30 @@ set -e
 SRC_REPO="/usr/share/maven-repo"
 DEST_REPO="$1/maven-repo"
 
-find_all_poms() {
-  find $SRC_REPO -name '*.pom' -printf '%P\n'
+find_src_poms() {
+  find -L $SRC_REPO -name '*.pom' -printf '%P\n'
+}
+
+list_fakes()
+{
+  CONFFILES="/etc/maven2/fake-poms.conf"
+  if [ -r debian/fake-poms.conf ]
+  then
+    CONFFILES="$CONFFILES debian/fake-poms.conf"
+  fi
+  sed -e's,#.*,,' $CONFFILES
+}
+
+find_dest_poms() {
+  find -L $DEST_REPO -name '*.pom' -printf '%P\n'
+}
+
+find_all_meta() {
+  find $DEST_REPO -name 'maven-metadata-tmp.xml'
 }
 
 echo_property() {
-  KEY=$(echo $BASE_DIR | tr / .)
+  KEY=$(echo $BASEDIR | tr / .)
   case "$KEY" in
     *.maven-*-plugin|*-maven-plugin)
       echo "$KEY.version = $VERSION"
@@ -21,10 +39,6 @@ echo_property() {
       return
       ;;
   esac
-}
-
-find_all_meta() {
-  find $DEST_REPO -name 'maven-metadata-tmp.xml'
 }
 
 header() {
@@ -51,17 +65,50 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-find_all_poms | while read POM; do
+find_src_poms | while read POM; do
+  VER_DIR=$(dirname $POM)
+  BASEDIR=$(dirname $VER_DIR)
+  mkdir -p $DEST_REPO/$BASEDIR
+  ln -s $SRC_REPO/$VER_DIR $DEST_REPO/$BASEDIR/
+done
+
+list_fakes | while read GROUPID ARTIFACTID JARFILE VERSION
+do
+  GROUPDIR=$(echo $GROUPID | tr . /)
+  BASEDIR="$DEST_REPO/$GROUPDIR/$ARTIFACTID"
+  JARFILE=${JARFILE:-"/usr/share/java/$ARTIFACTID.jar"}
+  VERSION=${VERSION:-"debian"}
+  if [ -d $BASEDIR/$VERSION ]
+  then
+    echo "skip faking of existing $GROUPID:$ARTIFACTID::$VERSION"
+    continue
+  else
+    mkdir -p $BASEDIR/$VERSION/
+  fi
+  if [ -r $JARFILE ]
+  then
+    PACKAGING="jar"
+    ln -s $JARFILE $BASEDIR/$VERSION/$ARTIFACTID-$VERSION.jar
+  else
+    PACKAGING="pom"
+  fi
+  cat > $BASEDIR/$VERSION/$ARTIFACTID-$VERSION.pom <<.EOF
+<project>
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>$GROUPID</groupId>
+  <artifactId>$ARTIFACTID</artifactId>
+  <version>$VERSION</version>
+  <packaging>$PACKAGING</packaging>
+</project>
+.EOF
+done
+
+find_dest_poms | while read POM; do
   VER_DIR=$(dirname $POM)
   VERSION=$(basename $VER_DIR)
-  BASE_DIR=$(dirname $VER_DIR)
-
-  mkdir -p $DEST_REPO/$BASE_DIR
-  ln -s $SRC_REPO/$VER_DIR $DEST_REPO/$BASE_DIR/
-  
+  BASEDIR=$(dirname $VER_DIR)
   VER_TAG="      <version>$VERSION</version>"
-  echo "$VER_TAG" >> $DEST_REPO/$BASE_DIR/maven-metadata-tmp.xml
-  
+  echo "$VER_TAG" >> $DEST_REPO/$BASEDIR/maven-metadata-tmp.xml
   echo_property
 done > $1/auto.properties
 
