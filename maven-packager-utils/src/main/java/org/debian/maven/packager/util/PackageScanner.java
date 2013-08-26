mans0954/maven-debian-helper

commit a16544f65571ebe092184330fffb3fa8d921fa8c

add classes for Debian Dependencies

maven-packager-utils/src/main/java/org/debian/maven/packager/util/PackageScanner.java
@@ -19,9 +19,13 @@ package org.debian.maven.packager.util;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.debian.maven.packager.DebianDependency;
+import org.debian.maven.repo.Dependency;
 
 public class PackageScanner {
 
@@ -34,12 +38,12 @@ public class PackageScanner {
         this.offline = offline;
     }
 
-    public String searchPkg(File dir, String extension) {
+    public DebianDependency searchPkg(File dir, String extension) {
         GetPackageContainingPatternResult packageResult = new GetPackageContainingPatternResult(extension);
         File cacheId = new File(dir, "<ANY>" + extension);
 
         if (filesInPackages.containsKey(cacheId)) {
-            return filesInPackages.get(cacheId);
+            return new DebianDependency(filesInPackages.get(cacheId));
         }
 
         IOUtil.executeProcess(new String[]{"dpkg", "--search", dir.getAbsolutePath() + "/*/*"}, packageResult);
@@ -48,7 +52,7 @@ public class PackageScanner {
         if (!packageResult.getPackages().isEmpty()) {
             pkg = packageResult.getPackages().iterator().next();
             filesInPackages.put(cacheId, pkg);
-            return pkg;
+            return new DebianDependency(pkg);
         }
 
         // Debian policy prevents the use of apt-file during a build
@@ -66,15 +70,19 @@ public class PackageScanner {
             pkg = packageResult.getPackages().iterator().next();
             filesInPackages.put(cacheId, pkg);
         }
-        return pkg;
+        return new DebianDependency(pkg);
 
     }
 
-    public String searchPkg(File fileToSearch) {
+    public DebianDependency searchJavaDocPkg(DebianDependency dependency) {
+        return searchPkg(new File("/usr/share/doc/" + dependency.getPackageName() + "/api/index.html"));
+    }
+
+    public DebianDependency searchPkg(File fileToSearch) {
         GetPackageResult packageResult = new GetPackageResult();
 
         if (filesInPackages.containsKey(fileToSearch)) {
-            return filesInPackages.get(fileToSearch);
+            return new DebianDependency(filesInPackages.get(fileToSearch));
         }
 
         String pkg = null;
@@ -82,7 +90,7 @@ public class PackageScanner {
         if (!packageResult.getResult().isEmpty()) {
             pkg = packageResult.getResult().iterator().next();
             filesInPackages.put(fileToSearch, pkg);
-            return pkg;
+            return new DebianDependency(pkg);
         }
 
         // Debian policy prevents the use of apt-file during a build
@@ -100,18 +108,18 @@ public class PackageScanner {
             pkg = packageResult.getResult().iterator().next();
             filesInPackages.put(fileToSearch, pkg);
         }
-        return pkg;
+        return new DebianDependency(pkg);
     }
 
-    public String getPackageVersion(String pkg, boolean onlyInstalled) {
+    public String getPackageVersion(DebianDependency pkg, boolean onlyInstalled) {
         GetPackageVersionResult packageResult = new GetPackageVersionResult();
-        IOUtil.executeProcess(new String[]{"dpkg", "--status", pkg}, packageResult);
+        IOUtil.executeProcess(new String[]{"dpkg", "--status", pkg.getPackageName()}, packageResult);
         if (packageResult.getResult() != null) {
             return packageResult.getResult();
         }
         if (!onlyInstalled) {
-            GetChangelogVersionResult versionResult = new GetChangelogVersionResult(pkg);
-            IOUtil.executeProcess(new String[]{"apt-get", "--no-act", "--verbose-versions", "install", pkg}, versionResult);
+            GetChangelogVersionResult versionResult = new GetChangelogVersionResult(pkg.getPackageName());
+            IOUtil.executeProcess(new String[]{"apt-get", "--no-act", "--verbose-versions", "install", pkg.getPackageName()}, versionResult);
             if (versionResult.getResult() != null) {
                 return versionResult.getResult();
             }
@@ -138,6 +146,22 @@ public class PackageScanner {
 
     public void makeExecutable(String file) {
         IOUtil.executeProcess(new String[]{"chmod", "+x", file}, new NoOutputHandler());
+    }
+
+    public ArrayList<DebianDependency> addDocDependencies(Collection<DebianDependency> debianDeps, Map<DebianDependency,
+        Dependency> versionedPackagesAndDependencies) {
+        ArrayList<DebianDependency> docDeps = new ArrayList<DebianDependency>();
+        for (DebianDependency dependency : debianDeps) {
+            Dependency runtimeDependency = versionedPackagesAndDependencies.get(dependency);
+            if (runtimeDependency != null && "pom".equals(runtimeDependency.getType())) {
+                continue;
+            }
+            DebianDependency docPkg = searchJavaDocPkg(dependency);
+            if (docPkg != null) {
+                docDeps.add(docPkg);
+            }
+        }
+        return docDeps;
     }
 
 }

maven-packager-utils/src/main/java/org/debian/maven/packager/DebianDependencies.java
@@ -1,0 +1,54 @@
+package org.debian.maven.packager;
+
+import java.util.Collection;
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import org.debian.maven.util.Strings;
+
+public class DebianDependencies {
+
+    public final EnumMap<Type, Set<DebianDependency>> deps = new EnumMap<Type, Set<DebianDependency>>(Type.class);
+
+    public DebianDependencies() {
+        for(Type type : Type.values()) {
+            deps.put(type, new HashSet<DebianDependency>());
+        }
+    }
+
+    public void add(Type type, DebianDependency dependency) {
+        this.deps.get(type).add(dependency);
+    }
+
+    public void add (Type type, Collection<DebianDependency> dependencies) {
+        get(type).addAll(dependencies);
+    }
+
+    public Set<DebianDependency> get(Type type) {
+        return deps.get(type);
+    }
+
+    public void putInProperties(Properties depVars) {
+        for(Type type : Type.values()) {
+            depVars.put(type.substvarName, Strings.join(deps.get(type), ", "));
+        }
+    }
+
+    public static enum Type {
+        COMPILE("maven.CompileDepends"),
+        TEST("maven.TestDepends"),
+        RUNTIME("maven.Depends"),
+        OPTIONAL("maven.OptionalDepends"),
+        DOC_RUNTIME("maven.DocDepends"),
+        DOC_OPTIONAL("maven.DocOptionalDepends");
+
+        public final String substvarName;
+
+        Type(String substvarName) {
+            this.substvarName = substvarName;
+        }
+    }
+
+}

maven-packager-utils/src/main/java/org/debian/maven/packager/DebianDependency.java
@@ -1,0 +1,53 @@
+package org.debian.maven.packager;
+
+public class DebianDependency {
+
+    private final String packageName;
+    private final String minimumVersion;
+
+    public DebianDependency(String pkgname, String minimumVersion) {
+        super();
+        if(pkgname.contains(" ")) throw new IllegalArgumentException("Not a valid package name: " + pkgname);
+        this.packageName = pkgname;
+        this.minimumVersion = minimumVersion;
+    }
+
+    public DebianDependency(String pkgname) {
+        this(pkgname, "");
+    }
+
+    @Override
+    public String toString() {
+        if(minimumVersion.isEmpty())
+            return packageName;
+        return packageName + " (>= " + minimumVersion + ")";
+    }
+
+    public String getPackageName() {
+        return packageName;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((minimumVersion == null) ? 0 : minimumVersion.hashCode());
+        result = prime * result + ((packageName == null) ? 0 : packageName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        DebianDependency other = (DebianDependency) obj;
+        if (minimumVersion == null) {
+            if (other.minimumVersion != null) return false;
+        } else if (!minimumVersion.equals(other.minimumVersion)) return false;
+        if (packageName == null) {
+            if (other.packageName != null) return false;
+        } else if (!packageName.equals(other.packageName)) return false;
+        return true;
+    }
+}

maven-packager-utils/src/main/java/org/debian/maven/packager/DependenciesSolver.java
@@ -17,10 +17,7 @@ package org.debian.maven.packager;
  * limitations under the License.
  */
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -48,6 +45,7 @@ import org.debian.maven.repo.Repository;
 import org.debian.maven.repo.Rule;
 import org.debian.maven.util.Strings;
 
+import static org.debian.maven.packager.DebianDependencies.Type.*;
 /**
  * Analyze the Maven dependencies and extract the Maven rules to use
  * as well as the list of dependent packages.
@@ -156,10 +154,7 @@ public class DependenciesSolver {
     private Set<Dependency> knownProjectDependencies = new TreeSet<Dependency>();
     private Set<Dependency> ignoredDependencies = new TreeSet<Dependency>();
     private Set<Dependency> notIgnoredDependencies = new TreeSet<Dependency>();
-    private Set<String> compileDepends = new TreeSet<String>();
-    private Set<String> testDepends = new TreeSet<String>();
-    private Set<String> runtimeDepends = new TreeSet<String>();
-    private Set<String> optionalDepends = new TreeSet<String>();
+    private DebianDependencies debianDeps = new DebianDependencies();
     private DependencyRuleSet cleanIgnoreRules = new DependencyRuleSet("Ignore rules to be applied during the Maven clean phase",
             new File("debian/maven.cleanIgnoreRules"));
     private boolean offline;
@@ -175,7 +170,7 @@ public class DependenciesSolver {
     // Keep the previous selected rule for a given version
     private Map<String, Rule> versionToRules = new HashMap<String, Rule>();
     // Keep the list of packages and dependencies
-    private Map<String, Dependency> versionedPackagesAndDependencies = new HashMap<String, Dependency>();
+    private Map<DebianDependency, Dependency> versionedPackagesAndDependencies = new HashMap<DebianDependency, Dependency>();
     private List<Rule> defaultRules = new ArrayList<Rule>();
     private PackageScanner scanner = new PackageScanner();
 
@@ -301,42 +296,12 @@ public class DependenciesSolver {
 
         if (generateJavadoc) {
             System.out.println("Checking dependencies for documentation packages...");
-            Set<String> docRuntimeDepends = new TreeSet<String>();
-            docRuntimeDepends.add("default-jdk-doc");
-            for (String dependency : runtimeDepends) {
-                Dependency runtimeDependency = versionedPackagesAndDependencies.get(dependency);
-                if (dependency.indexOf(' ') > 0) {
-                    dependency = dependency.substring(0, dependency.indexOf(' '));
-                }
-                if (runtimeDependency != null && "pom".equals(runtimeDependency.getType())) {
-                    continue;
-                }
-                String docPkg = scanner.searchPkg(new File("/usr/share/doc/" + dependency + "/api/index.html"));
-                if (docPkg != null) {
-                    docRuntimeDepends.add(docPkg);
-                }
-            }
-            Set<String> docOptionalDepends = new TreeSet<String>();
-            for (String dependency : optionalDepends) {
-                Dependency optionalDependency = versionedPackagesAndDependencies.get(dependency);
-                if (dependency.indexOf(' ') > 0) {
-                    dependency = dependency.substring(0, dependency.indexOf(' '));
-                }
-                if (optionalDependency != null && "pom".equals(optionalDependency.getType())) {
-                    continue;
-                }
-                String docPkg = scanner.searchPkg(new File("/usr/share/doc/" + dependency + "/api/index.html"));
-                if (docPkg != null) {
-                    docOptionalDepends.add(docPkg);
-                }
-            }
 
-            depVars.put("maven.CompileDepends", Strings.join(compileDepends, ", "));
-            depVars.put("maven.TestDepends", Strings.join(testDepends, ", "));
-            depVars.put("maven.Depends", Strings.join(runtimeDepends, ", "));
-            depVars.put("maven.OptionalDepends", Strings.join(optionalDepends, ", "));
-            depVars.put("maven.DocDepends", Strings.join(docRuntimeDepends, ", "));
-            depVars.put("maven.DocOptionalDepends", Strings.join(docOptionalDepends, ", "));
+            debianDeps.add(DOC_RUNTIME, new DebianDependency("default-jdk-doc"));
+            debianDeps.add(DOC_RUNTIME, scanner.addDocDependencies(debianDeps.get(RUNTIME), versionedPackagesAndDependencies));
+            debianDeps.add(DOC_OPTIONAL, scanner.addDocDependencies(debianDeps.get(OPTIONAL), versionedPackagesAndDependencies));
+
+            debianDeps.putInProperties(depVars);
         }
         if (packageVersion != null) {
             depVars.put("maven.UpstreamPackageVersion", packageVersion);
@@ -923,7 +888,7 @@ public class DependenciesSolver {
                 }
                 return null;
             } else {
-                String pkg = scanner.searchPkg(new File("/usr/share/maven-repo/"
+                DebianDependency pkg = scanner.searchPkg(new File("/usr/share/maven-repo/"
                         + dependency.getGroupId().replace('.', '/')
                         + "/" + dependency.getArtifactId()), ".pom");
                 if (pkg != null) {
@@ -999,39 +964,41 @@ public class DependenciesSolver {
         }
 
         // Discover the library to import for the dependency
-        String pkg = getPackage(pom, sourcePomLoc);
+        DebianDependency pkg = getPackage(pom, sourcePomLoc);
 
         if (pkg != null && !pkg.equals(packageName)) {
-            String libraryWithVersionConstraint = pkg;
-            String version = dependency.getVersion();
-            if (version == null || (pom.getOriginalVersion() != null && version.compareTo(pom.getOriginalVersion()) > 0)) {
-                version = pom.getOriginalVersion();
-            }
+            DebianDependency libraryWithVersionConstraint;
             if (pom.getOriginalVersion() != null && (pom.getProperties().containsKey("debian.hasPackageVersion"))) {
-                libraryWithVersionConstraint += " (>= " + version + ")";
+                String version = dependency.getVersion();
+                if (version == null || (pom.getOriginalVersion() != null && version.compareTo(pom.getOriginalVersion()) > 0)) {
+                    version = pom.getOriginalVersion();
+                }
+                libraryWithVersionConstraint = new DebianDependency(pkg.getPackageName(), version);
+            } else {
+                libraryWithVersionConstraint = pkg;
             }
             if (!management) {
                 if (buildTime) {
                     if ("test".equals(dependency.getScope())) {
-                        testDepends.add(libraryWithVersionConstraint);
+                        debianDeps.add(TEST, libraryWithVersionConstraint);
                     } else if ("maven-plugin".equals(dependency.getType())) {
                         if (!packageType.equals("ant")) {
-                            compileDepends.add(libraryWithVersionConstraint);
+                            debianDeps.add(COMPILE, libraryWithVersionConstraint);
                         }
                     } else if (mavenExtension) {
                         if (!packageType.equals("ant")) {
-                            compileDepends.add(libraryWithVersionConstraint);
+                            debianDeps.add(COMPILE, libraryWithVersionConstraint);
                         }
                     } else {
-                        compileDepends.add(libraryWithVersionConstraint);
+                        debianDeps.add(COMPILE, libraryWithVersionConstraint);
                     }
                 } else {
                     if (dependency.isOptional()) {
-                        optionalDepends.add(libraryWithVersionConstraint);
+                        debianDeps.add(OPTIONAL, libraryWithVersionConstraint);
                     } else if ("test".equals(dependency.getScope())) {
-                        testDepends.add(libraryWithVersionConstraint);
+                        debianDeps.add(TEST, libraryWithVersionConstraint);
                     } else {
-                        runtimeDepends.add(libraryWithVersionConstraint);
+                        debianDeps.add(RUNTIME, libraryWithVersionConstraint);
                     }
                 }
             }
@@ -1064,10 +1031,10 @@ public class DependenciesSolver {
         return pom.getThisPom();
     }
 
-    private String getPackage(POMInfo pom, String sourcePomLoc) {
-        String pkg = null;
+    private DebianDependency getPackage(POMInfo pom, String sourcePomLoc) {
+        DebianDependency pkg = null;
         if (pom.getProperties() != null) {
-            pkg = pom.getProperties().get("debian.package");
+            pkg = new DebianDependency(pom.getProperties().get("debian.package"));
         }
         if (pkg == null) {
             Dependency dependency = pom.getThisPom();

commit b2883b552bcfee8fc20d14caeec49a5b10b763e3

Changed PackageScanner.searchPkg(File) to return null instead of throwing an exception when no package is found (broken since commit a16544f)

maven-packager-utils/src/main/java/org/debian/maven/packager/util/PackageScanner.java
@@ -42,18 +42,18 @@ public class PackageScanner {
     }
 
     public DebianDependency searchPkg(File dir, String extension) {
-        GetPackageContainingPatternResult packageResult = new GetPackageContainingPatternResult(extension);
-        File cacheId = new File(dir, "<ANY>" + extension);
-
+        // lookup the cache first
+        File cacheId = new File(dir, "<ANY>" + extension);        
         if (filesInPackages.containsKey(cacheId)) {
             return new DebianDependency(filesInPackages.get(cacheId));
         }
 
+        GetPackageContainingPatternResult packageResult = new GetPackageContainingPatternResult(extension);
+
         IOUtil.executeProcess(new String[]{"dpkg", "--search", dir.getAbsolutePath() + "/*/*"}, packageResult);
 
-        String pkg = null;
         if (!packageResult.getPackages().isEmpty()) {
-            pkg = packageResult.getPackages().iterator().next();
+            String pkg = packageResult.getPackages().iterator().next();
             filesInPackages.put(cacheId, pkg);
             return new DebianDependency(pkg);
         }
@@ -70,10 +70,12 @@ public class PackageScanner {
         }
         IOUtil.executeProcess(new String[]{"apt-file", "search", dir.getAbsolutePath()}, packageResult);
         if (!packageResult.getPackages().isEmpty()) {
-            pkg = packageResult.getPackages().iterator().next();
+            String pkg = packageResult.getPackages().iterator().next();
             filesInPackages.put(cacheId, pkg);
+            new DebianDependency(pkg);
         }
-        return pkg == null ? null : new DebianDependency(pkg);
+        
+        return null; 
     }
 
 
@@ -92,16 +94,16 @@ public class PackageScanner {
     }
 
     public DebianDependency searchPkg(File fileToSearch) {
-        GetPackageResult packageResult = new GetPackageResult();
-
+        // lookup the cache first
         if (filesInPackages.containsKey(fileToSearch)) {
             return new DebianDependency(filesInPackages.get(fileToSearch));
         }
 
-        String pkg = null;
+        GetPackageResult packageResult = new GetPackageResult();
+
         IOUtil.executeProcess(new String[]{"dpkg", "--search", fileToSearch.getAbsolutePath()}, packageResult);
         if (!packageResult.getResult().isEmpty()) {
-            pkg = packageResult.getResult().iterator().next();
+            String pkg = packageResult.getResult().iterator().next();
             filesInPackages.put(fileToSearch, pkg);
             return new DebianDependency(pkg);
         }
@@ -118,10 +120,12 @@ public class PackageScanner {
         }
         IOUtil.executeProcess(new String[]{"apt-file", "search", fileToSearch.getAbsolutePath()}, packageResult);
         if (!packageResult.getResult().isEmpty()) {
-            pkg = packageResult.getResult().iterator().next();
+            String pkg = packageResult.getResult().iterator().next();
             filesInPackages.put(fileToSearch, pkg);
+            return new DebianDependency(pkg);
         }
-        return new DebianDependency(pkg);
+        
+        return null;
     }
 
     public String getPackageVersion(DebianDependency pkg, boolean onlyInstalled) {
@@ -151,8 +155,7 @@ public class PackageScanner {
         }
         System.out.println();
         System.out.println("Looking for shared jars in package " + library + "...");
-        IOUtil.executeProcess(new String[]{"dpkg", "--listfiles", library},
-                new SharedJarOutputHandler(jars));
+        IOUtil.executeProcess(new String[]{"dpkg", "--listfiles", library}, new SharedJarOutputHandler(jars));
         cacheOfSharedJars.put(library, jars);
         return jars;
     }