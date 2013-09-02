576884684f82e213a771c4c49d233e4ced2f7ac2
Emmanuel Bourg <ebourg@apache.org>
2013-09-02 18:34:58 +0200

    Added more tests for DebianDependency

--- maven-packager-utils/src/test/java/org/debian/maven/packager/DebianDependencyTest.java
+++ maven-packager-utils/src/test/java/org/debian/maven/packager/DebianDependencyTest.java
@@ -31,8 +31,40 @@ public class DebianDependencyTest {
         assertEquals(packageName, checkPackageName(packageName));
     }
 
-    @Test(expected=NullPointerException.class)
+    @Test(expected=IllegalArgumentException.class)
     public void testCheckPackageNameDoesNotAcceptNull() {
         checkPackageName(null);
+    }
+
+    @Test
+    public void testEquals() {
+        DebianDependency dep1 = new DebianDependency("maven-debian-helper", "1.0");
+        DebianDependency dep2 = new DebianDependency("maven-debian-helper", "1.0");
+        DebianDependency dep3 = new DebianDependency("maven-repo-helper", "1.0");
+        DebianDependency dep4 = new DebianDependency("maven-repo-helper", "2.0");
+        
+        assertFalse(dep1.equals(""));
+        assertTrue(dep1.equals(dep1));
+        assertTrue(dep1.equals(dep2));
+        assertFalse(dep2.equals(dep3));
+        assertFalse(dep3.equals(dep4));
+    }
+
+    @Test
+    public void testToString() {
+        DebianDependency dep1 = new DebianDependency("maven-debian-helper", "1.0");
+        assertEquals("maven-debian-helper (>= 1.0)", dep1.toString());
+        
+        DebianDependency dep2 = new DebianDependency("maven-debian-helper");
+        assertEquals("maven-debian-helper", dep2.toString());
+    }
+    
+    @Test
+    public void testCompare() {
+        DebianDependency dep1 = new DebianDependency("maven-debian-helper", "1.0");
+        DebianDependency dep2 = new DebianDependency("maven-repo-helper", "1.0");
+        
+        assertEquals(0, dep1.compareTo(dep1));
+        assertTrue(dep1.compareTo(dep2) < 0);
     }
 }
